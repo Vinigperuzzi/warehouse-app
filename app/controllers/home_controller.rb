bc33958dde90e84df66a67b147dd409d88f433da
Vinigperuzzi <vinigperuzzi@hotmail.com>
2024-04-06 14:07:14 -0300

    ADD: galpões são listados na homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+    @warehouses = Warehouse.all
   end
 
 end
--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -8,4 +8,28 @@ describe 'Usuário visita tela inicial' do
     # Assert
     expect(page).to have_content('Galpões & Estoque')
   end
+
+  it 'e vê os galpões cadastrados' do
+    # Arrange
+    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
+    Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000)
+    # Act
+    visit('/')
+    # Assert
+    expect(page).not_to have_content('Não há galpões cadastrados.')
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60000 m²')
+
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('Código: MCZ')
+    expect(page).to have_content('Cidade: Maceio')
+    expect(page).to have_content('50000 m²')
+  end
+
+  it 'e não tem galpões cadastrados' do
+    visit('/')
+    expect(page).to have_content('Não há galpões cadastrados.')
+  end
 end
